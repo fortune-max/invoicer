0a62042b7b60feb28d4e56b90c049484d8ff1e49
lordfme <fortmax120@gmail.com>
2022-05-10 13:13:01 +0100

    allow for investor_id query param in cashcall/ and investment/

--- invoice/views.py
+++ invoice/views.py
@@ -4,17 +4,29 @@ from .models import CashCall, Investment, Investor, Bill
 from invoice.serializer import BillSerializer, CashCallSerializer, InvestmentSerializer, InvestorSerializer
 
 
-class CashCallViewSet(viewsets.ModelViewSet):
-    queryset = CashCall.objects.all()
-    serializer_class = CashCallSerializer
-
 class InvestorViewSet(viewsets.ModelViewSet):
     queryset = Investor.objects.all()
     serializer_class = InvestorSerializer
 
+class CashCallViewSet(viewsets.ModelViewSet):
+    serializer_class = CashCallSerializer
+
+    def get_queryset(self):
+        investor_id = self.request.GET.get("investor_id")
+        if investor_id:
+            investor = get_object_or_404(Investor, pk=investor_id)
+            return CashCall.objects.filter(investor=investor)
+        return CashCall.objects.all()
+
 class InvestmentViewSet(viewsets.ModelViewSet):
-    queryset = Investment.objects.all()
     serializer_class = InvestmentSerializer
+
+    def get_queryset(self):
+        investor_id = self.request.GET.get("investor_id")
+        if investor_id:
+            investor = get_object_or_404(Investor, pk=investor_id)
+            return Investment.objects.filter(investor=investor)
+        return Investment.objects.all()
 
 class BillViewSet(viewsets.ModelViewSet):
     serializer_class = BillSerializer
--- invoicer/urls.py
+++ invoicer/urls.py
@@ -17,7 +17,6 @@ from django.contrib import admin
 from django.urls import include, path
 
 urlpatterns = [
-    # path('cashcall/', include('cashcall.urls')),
     path('admin/', admin.site.urls),
     path('api-auth/', include('rest_framework.urls')),
     path("invoice/", include("invoice.urls")),
