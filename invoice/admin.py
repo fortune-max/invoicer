41b1ec7e210c17af0d8df4c65a24b6007785b3e9
lordfme <fortmax120@gmail.com>
2022-05-10 19:51:31 +0100

    create corresponding bill when investment created

--- invoice/admin.py
+++ invoice/admin.py
@@ -3,6 +3,7 @@ from django.contrib import admin
 from django.db.models.signals import post_save
 from django.dispatch import receiver
 from . import models
+from .utils import get_cashcall, calc_amount
 
 # Model registration
 admin.site.register(models.Investor)
@@ -26,12 +27,22 @@ def bill_membership(sender, instance, created, **kwargs):
             amount = 0,
             investor = instance,
             validated = True,
-            invalid = False,
             fulfilled_one = True,
             fulfilled_all = True,
             cashcall = membership_cashcall,
-            date = date.today(),
         )
         membership_cashcall.save()
         membership_bill.save()
-        
+
+@receiver(post_save, sender=models.Investment)
+def bill_investment(sender, instance, created, **kwargs):
+    if created:
+        investment_cashcall = get_cashcall(instance.investor, False)
+        investment_bill = models.Bill(
+            frequency = "Y1",
+            bill_type = "INVESTMENT",
+            amount = calc_amount(instance, 1),
+            investor = instance.investor,
+            cashcall = investment_cashcall,
+        )
+        investment_bill.save()
