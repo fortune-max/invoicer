6740d730cb354898143f0d1e66215e9eab1a08b6
lordfme <fortmax120@gmail.com>
2022-05-10 22:56:08 +0100

    add validated, sent and fulfilled query params to endpoints

--- invoice/views.py
+++ invoice/views.py
@@ -1,4 +1,5 @@
 from rest_framework import viewsets
+from ast import literal_eval as safe_eval
 from django.shortcuts import get_object_or_404
 from .models import CashCall, Investment, Investor, Bill
 from invoice.serializer import BillSerializer, CashCallSerializer, InvestmentSerializer, InvestorSerializer
@@ -12,28 +13,52 @@ class CashCallViewSet(viewsets.ModelViewSet):
     serializer_class = CashCallSerializer
 
     def get_queryset(self):
+        queryset = CashCall.objects.all()
+        sent = self.request.GET.get("sent")
+        fulfilled = self.request.GET.get("fulfilled")
+        validated = self.request.GET.get("validated")
         investor_id = self.request.GET.get("investor_id")
         if investor_id:
             investor = get_object_or_404(Investor, pk=investor_id)
-            return CashCall.objects.filter(investor=investor)
-        return CashCall.objects.all()
+            queryset = queryset.filter(investor=investor)
+        if fulfilled:
+            queryset = [cashcall for cashcall in queryset if cashcall.fulfilled==safe_eval(fulfilled)]
+        if validated:
+            queryset = [cashcall for cashcall in queryset if cashcall.validated==safe_eval(validated)]
+        if sent:
+            queryset = queryset.filter(sent=sent)
+        return queryset
 
 class InvestmentViewSet(viewsets.ModelViewSet):
     serializer_class = InvestmentSerializer
 
     def get_queryset(self):
+        queryset = Investment.objects.all()
+        fulfilled = self.request.GET.get("fulfilled")
         investor_id = self.request.GET.get("investor_id")
         if investor_id:
             investor = get_object_or_404(Investor, pk=investor_id)
-            return Investment.objects.filter(investor=investor)
+            queryset = queryset.filter(investor=investor)
+        if fulfilled:
+            queryset = [investment for investment in queryset if investment.fulfilled==safe_eval(fulfilled)]
         return Investment.objects.all()
 
 class BillViewSet(viewsets.ModelViewSet):
     serializer_class = BillSerializer
 
     def get_queryset(self):
+        queryset = Bill.objects.all()
+        sent = self.request.GET.get("sent")
+        fulfilled = self.request.GET.get("fulfilled")
+        validated = self.request.GET.get("validated")
         investor_id = self.request.GET.get("investor_id")
         if investor_id:
             investor = get_object_or_404(Investor, pk=investor_id)
-            return Bill.objects.filter(investor=investor)
-        return Bill.objects.all()
+            queryset = queryset.filter(investor=investor)
+        if fulfilled:
+            queryset = queryset.filter(fulfilled_one=fulfilled)
+        if validated:
+            queryset = queryset.filter(validated=validated)
+        if sent:
+            queryset = [bill for bill in queryset if bill.cashcall.sent==safe_eval(sent)]
+        return queryset
