931601ad6a1cf43ea69a28a7b8b11a00ee79cb5a
lordfme <fortmax120@gmail.com>
2022-05-11 19:05:05 +0100

    update utils with helper fns

--- invoice/utils.py
+++ invoice/utils.py
@@ -1,13 +1,17 @@
-from datetime import date
+import calendar
 from decimal import Decimal
-from .models import CashCall, Investment, Investor
+from datetime import date, timedelta
+from .models import CashCall, Investment, Investor, Bill
+
+dcm = lambda x: Decimal(str(x))
+days_in_year = lambda year: 365 + calendar.isleap(year)
 
 def get_cashcall(investor: Investor, validated: bool):
     """
     Returns the first cashcall for supplied investor that is/is not validated.
     Creates and saves a cashcall if none exists. Helps in grouping bills to appropriate cashcalls.
     """
-    not_sent = CashCall.objects.filter(investor=investor).filter(sent=False).all() # Filter cashcalls by investor not yet sent out
+    not_sent = CashCall.objects.filter(investor=investor).filter(sent=False).all() # Take cashcalls for investor and not yet sent
     not_sent = [cashcall for cashcall in not_sent if cashcall.validated==validated or cashcall.bill_count==0] # match validity
     if not_sent:
         return sorted(not_sent, key=lambda x: x.bill_count, reverse=True)[0] # Prioritize non-empty cashcalls to append bill to
@@ -15,8 +19,8 @@ def get_cashcall(investor: Investor, validated: bool):
     new_cashcall.save()
     return new_cashcall
 
-dcm = lambda x: Decimal(str(x))
-def calc_amount(investment: Investment, instalment_no: int):
+
+def calc_amount_due_investment(investment: Investment, instalment_no: int):
     """
     Get the amount due for an investment given the instalment no. (year)
     """
@@ -36,3 +40,41 @@ def calc_amount(investment: Investment, instalment_no: int):
         amount = (num_of_days / days_in_year) * (investment.fee_percent - discount) / 100 * investment.total_amount
     amount = (investment.fee_percent - discount) / 100 * investment.total_amount
     return min(amount, investment.amount_left)
+
+
+def yearly_spend(investor: Investor, start_date:date, years_back: int):
+    """
+    Get amount spent by an investor from {start_year-years_back} to {start_year}
+    """
+    years = range(start_date.year - years_back, start_date.year + 1)
+    days = sum([days_in_year(year) for year in years])
+    period_start = start_date - timedelta(days=days)
+    relevant_bills = Bill.objects.filter(investor=investor, date__gt=period_start, date__lt=start_date)
+    amount_spent = sum([bill.amount for bill in relevant_bills])
+    return amount_spent
+
+
+def calc_amount_due_membership(investor: Investor, pro_rata_days=0):
+    """
+    Get membership amount due. Accounts for waiving if over yearly spend.
+    Also accounts for membership deactivation by pro-rata billing.
+    """
+    year_rates = {
+        date(2050, 4, 1): {"membership": dcm(50_000), "membership_waive": dcm(100_000)},
+        date(2030, 6, 1): {"membership": dcm(25_000), "membership_waive": dcm(50_000)},
+        date(1900, 1, 1): {"membership": dcm(3_000), "membership_waive": dcm(50_000)},
+    } # dates are lower limits, and are the dates yearly membership bills were changed. Sorted newest to oldest.
+    for date_obj, yearly_fee in year_rates.items():
+        if date.today() >= date_obj:
+            membership_fee = yearly_fee["membership"]
+            membership_waive = yearly_fee["membership_waive"]
+            break
+    # Spent over fee threshold within year
+    if yearly_spend(investor=investor,start_date=date.today(), years_back=1) >= membership_waive:
+        return Decimal('0')
+    # Handle membership billing prorata on deactivation of account
+    if pro_rata_days:
+        start_date = date.today() - timedelta(days=pro_rata_days)
+        return dcm(pro_rata_days)/days_in_year(start_date) * membership_fee
+    # Handle regular membership
+    return membership_fee
